f695e8e52432c8bccfd2515068128e962cc2aace
ZohirEDDIG <zohireddig.dev@gmail.com>
2025-09-23 20:16:25 +0100

    Fix footer in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,11 +12,11 @@ const Home = () => {
         <>
             <Header />
 
-            <main className='bg-gray-300 dark:bg-gray-900 min-h-[calc(100dvh-53.46px)] mt-[53.46px]'>
+            <main className='bg-gray-300 dark:bg-gray-900 mt-[53.46px]'>
 
                 <Sidebar />
 
-                <div className={`p-4 ${isSidebarOpen ? 'ml-[60px] lg:ml-[300px]' : 'ml-[60px]'} flex flex-col gap-y-6 transition-[margin-left] duration-300 ease-in-out`}>
+                <div className={`min-h-[calc(100dvh-53.46px)] p-4 ${isSidebarOpen ? 'ml-[60px] lg:ml-[300px]' : 'ml-[60px]'} flex flex-col gap-y-6 transition-[margin-left] duration-300 ease-in-out`}>
                   
                     <h1 className='dark:text-white sm:text-xl'>{t('hd_wallpapers_free_desktop_backgrounds')}</h1>
 
@@ -26,9 +26,10 @@ const Home = () => {
 
                     <Pagination />
 
-                    <Footer />
 
                 </div>
+                
+                <Footer />
 
             </main>
 
